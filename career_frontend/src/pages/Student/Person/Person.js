03e1ae0fe23558777bd3aff3dc55997e034d2cc8
alisa yuan <alisa.yuan@ihr360.com>
2020-09-08 14:28:51 +0800

    gengixn

--- career_frontend/src/pages/Student/Person/Person.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PageHeaderWrapper from '@/components/PageHeaderWrapper';
-import PersonDetail from './PersonDetail';
-
-export default () => (
-  <PageHeaderWrapper title="个人信息">
-    <PersonDetail />
-  </PageHeaderWrapper>
-);
--- career_frontend/src/services/teacher/studentRecord.js
+++
@@ -1,6 +0,0 @@
-import { stringify } from 'qs';
-import request from '@/utils/request';
-
-export async function getStudentRecord(params) {
-  return request(`/user/Teacherindex/apiSelectTeacherStudent?${stringify(params)}`);
-}
